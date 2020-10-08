64cdea0287b85a9ad69ec2a6c4c2f63b7581e5b5
guest0417 <48578453+guest0417@users.noreply.github.com>
2020-10-08 22:10:49 +0800

    Update game_manager.js

--- GetSJTU!_files/game_manager.js
+++ GetSJTU!_files/game_manager.js
@@ -140,8 +140,8 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
-          // if (merged.value === 1024) self.won = true;
+          // if (merged.value === 2048) self.won = true;
+          if (merged.value === 1024) self.won = true;
           // if (merged.value === 512) self.won = true;
           // if (merged.value === 256) self.won = true;
         } else {
